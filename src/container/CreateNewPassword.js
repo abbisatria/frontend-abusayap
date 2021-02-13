7b0280771c8730f84ca93a1b637a3627c6ff02c5
shafanaura <shafanaura48@gmail.com>
2021-02-13 12:08:55 +0700

    change icon add auth page

--- src/container/CreateNewPassword.js
+++ src/container/CreateNewPassword.js
@@ -21,10 +21,16 @@ Password In a Minutes.</p>
               Now you can create a new password for your Zwallet account. Type your password twice so we can confirm your new passsword.</p>
             <Form>
               <FormInput div="py-3" group="inputWithIcon" type="password" placeholder="Enter your password">
-                <i className="fa fa-lock fa-lg fa-fw" aria-hidden="true"></i>
+                <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" className="bi bi-key" viewBox="0 0 16 16">
+                  <path d="M0 8a4 4 0 0 1 7.465-2H14a.5.5 0 0 1 .354.146l1.5 1.5a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0L13 9.207l-.646.647a.5.5 0 0 1-.708 0L11 9.207l-.646.647a.5.5 0 0 1-.708 0L9 9.207l-.646.647A.5.5 0 0 1 8 10h-.535A4 4 0 0 1 0 8zm4-3a3 3 0 1 0 2.712 4.285A.5.5 0 0 1 7.163 9h.63l.853-.854a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .708 0l.646.647.793-.793-1-1h-6.63a.5.5 0 0 1-.451-.285A3 3 0 0 0 4 5z"/>
+                  <path d="M4 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+                </svg>
               </FormInput>
               <FormInput div="pb-5" group="inputWithIcon" type="password" placeholder="Enter your password">
-                <i className="fa fa-lock fa-lg fa-fw" aria-hidden="true"></i>
+                <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" className="bi bi-key" viewBox="0 0 16 16">
+                  <path d="M0 8a4 4 0 0 1 7.465-2H14a.5.5 0 0 1 .354.146l1.5 1.5a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0L13 9.207l-.646.647a.5.5 0 0 1-.708 0L11 9.207l-.646.647a.5.5 0 0 1-.708 0L9 9.207l-.646.647A.5.5 0 0 1 8 10h-.535A4 4 0 0 1 0 8zm4-3a3 3 0 1 0 2.712 4.285A.5.5 0 0 1 7.163 9h.63l.853-.854a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .708 0l.646.647.793-.793-1-1h-6.63a.5.5 0 0 1-.451-.285A3 3 0 0 0 4 5z"/>
+                  <path d="M4 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+                </svg>
               </FormInput>
               <Link to='/login' className="pt-5">
                 <ButtonCustom block >
--- src/container/ResetPassword.js
+++ src/container/ResetPassword.js
@@ -21,7 +21,9 @@ Password In a Minutes.</p>
               To reset your password, you must type your e-mail and we will send a link to your email and you will be directed to the reset password screens.</p>
             <Form>
               <FormInput div="pt-3 pb-5" group="inputWithIcon" type="email" placeholder="Enter your e-mail">
-                <i className="fa fa-envelope fa-lg fa-fw" aria-hidden="true"></i>
+                <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" className="bi bi-envelope" viewBox="0 0 16 16">
+                  <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2zm13 2.383l-4.758 2.855L15 11.114v-5.73zm-.034 6.878L9.271 8.82 8 9.583 6.728 8.82l-5.694 3.44A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.739zM1 11.114l4.758-2.876L1 5.383v5.73z"/>
+                </svg>
               </FormInput>
               <Link to='/create-new-password' className="pt-5">
                 <ButtonCustom block >
